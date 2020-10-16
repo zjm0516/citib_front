67731b8286fa497a7fe0f83772d326568535d4aa
zhangjingmin <1553316823@qq.com>
2020-10-16 19:44:15 +0800

    first commit

--- cibit_front/src/components/HelloWorld.vue
+++ cibit_front/src/components/HelloWorld.vue
@@ -1,25 +1,55 @@
 <template>
+<!--  <div class="topMenu" >-->
+<!--    <el-menu-->
+<!--      :default-active="activeIndex2"-->
+<!--      class="el-menu-demo"-->
+<!--      mode="horizontal"-->
+<!--      @select="handleSelect"-->
+<!--      background-color="#545c64"-->
+<!--      text-color="#fff"-->
+<!--      active-text-color="#ffd04b">-->
+<!--      <el-menu-item index="1">处理中心</el-menu-item>-->
+<!--      <el-submenu index="2">-->
+<!--        <template slot="title">我的工作台</template>-->
+<!--        <el-menu-item index="2-1">选项1</el-menu-item>-->
+<!--        <el-menu-item index="2-2">选项2</el-menu-item>-->
+<!--        <el-menu-item index="2-3">选项3</el-menu-item>-->
+<!--        <el-submenu index="2-4">-->
+<!--          <template slot="title">选项4</template>-->
+<!--          <el-menu-item index="2-4-1">选项1</el-menu-item>-->
+<!--          <el-menu-item index="2-4-2">选项2</el-menu-item>-->
+<!--          <el-menu-item index="2-4-3">选项3</el-menu-item>-->
+<!--        </el-submenu>-->
+<!--      </el-submenu>-->
+<!--      <el-menu-item index="3" disabled>消息中心</el-menu-item>-->
+<!--      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>-->
+<!--    </el-menu>-->
+<!--  </div>-->
 
   <el-container>
     <!--  //最上面的一行表头-->
-    <el-header class="topestheader" style="height: 120px;">
+    <el-header class="topestheader" style="height: 120px;Float:left;">
       <el-image
         style="width: 100px; height: 100px;top: 10px;
-    right: 800px;"
-        :src="url"
-        :fit="fit"></el-image>
-      <div class="floatput" style="Float:left;height:30px;left: 800px;">
+    left: -40%"
+        :src=url
+        :fit=fit></el-image>
+      <div class="floatput"  style="Float:left;height:60px;margin-top: -4%;">
 
-        <el-input v-model="SearchParam" placeholder="请输入查询字段" style="width:300px"></el-input>
+        <el-input v-model="SearchParam" placeholder="请输入查询字段" style=" width:300px"></el-input>
         <el-button type="primary">搜索</el-button>
 
-      </div>
 
-      <div class="el-icon-user-solid" style="position: fixed;
+      </div>
+      <div class="el-icon-user-solid" style="
+    position: absolute;
     font-size: 40px;
-    top: 40px;
-    left: 1700px;}"></div>
+    left: 90%;
+    top: 40px;"></div>
+
+
     </el-header>
+
     <el-container>
       <!--      //左侧的部分-->
       <el-aside class="firstAside">
@@ -35,9 +65,33 @@
       <el-main>
         <el-container>
           <!--          //右下的小菜单的header-->
-          <el-header></el-header>
+          <el-header style="    height: 120px;" >
+            <div style="height: 60px">
+              <el-button style="margin-right: 10%;" @click="changeToCarbonTrading" type="primary" >碳交易</el-button>
+              <el-button style="margin-right: 10%;" @click="changeToCarbonfinance" type="primary" >碳金融</el-button>
+              <el-button style="margin-right: 10%;" @click="changeToPoliciesandregulations" type="primary" >政策法规</el-button>
+              <el-button style="margin-right: 10%;" @click="changeToWatchtoday" type="primary" >今日看点</el-button>
+            </div>
+
+
+
+          </el-header>
           <!--          //右下的主要不服-->
-          <el-main></el-main>
+          <el-main class="secondmain" >
+            <div v-show="CarbonTrading">
+              <el-tag type="danger">标签1</el-tag>
+            </div>
+            <div v-show="Carbonfinance">
+              <el-tag type="danger">标签2</el-tag>
+            </div>
+            <div v-show="Policiesandregulations">
+              <el-tag type="danger">标签3</el-tag>
+            </div>
+            <div v-show="Watchtoday">
+              <el-tag type="danger">标签4</el-tag>
+
+            </div>
+          </el-main>
 
         </el-container>
 
@@ -54,12 +108,50 @@
       return {
         fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
         url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-        msg: ""
+        msg: "",
+        SearchParam:"",
+        activeIndex: '1',
+        activeIndex2: '1',
+        CarbonTrading:true,
+        Carbonfinance:false,
+        Policiesandregulations:false,
+        Watchtoday:false
+
       }
     },
     methods: {
       searchIndb() {
 
+      },
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      changeToCarbonTrading()
+      {
+        this.CarbonTrading=true;
+        this.Carbonfinance=false;
+        this.Policiesandregulations=false;
+        this.Watchtoday=false;
+      },
+      changeToCarbonfinance()
+      {
+        this.CarbonTrading=false;
+        this.Carbonfinance=true;
+        this.Policiesandregulations=false;
+        this.Watchtoday=false;
+      },
+      changeToPoliciesandregulations(){
+        this.CarbonTrading=false;
+        this.Carbonfinance=false;
+        this.Policiesandregulations=true;
+        this.Watchtoday=false;
+
+      },
+      changeToWatchtoday(){
+        this.CarbonTrading=false;
+        this.Carbonfinance=false;
+        this.Policiesandregulations=false;
+        this.Watchtoday=true
       },
 
     }
@@ -67,19 +159,38 @@
 </script>
 
 <style scoped>
+  .erjiMenu{
+
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+
+
+  }
   .floatput {
-    position: fixed;
-    top: 40px;
-    left: 600px;
+
+
+
   }
 
   .el-header.topestheader{
     background-color: #B3C0D1;
     color: #333;
-    text-align: center;
+
 
   }
-  .el-header.
+  .el-header.thirdheader
+  {
+    background-color: #B3C0D1;
+    color: #333;
+    float: left;
+  }
+  .el-header{
+    background-color: #B3C0D1;
+    color: #333;
+    float: left;
+  }
   .el-footer.firstAside {
     background-color: #B3C0D1;
     color: #333;
@@ -96,6 +207,12 @@
   }
 
   .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  .el-main.secondmain{
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
