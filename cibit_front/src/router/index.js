a2f811b65baadbdfab1e2cf170d985d2f7ee3ea4
zhangjingmin <1553316823@qq.com>
2020-10-20 13:04:35 +0800

    新建看盘，咨询，我的界面

--- cibit_front/src/router/index.js
+++ cibit_front/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Consult from  '@/components/Consult'
+import Kanpan from  '@/components/Kanpan'
+import Login from  '@/components/Login'
+import Service from  '@/components/Service'
+import Mine from  '@/components/Mine'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +14,31 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Consult',
+      name: 'Consult',
+      component: Consult
+    },
+    {
+      path: '/Kanpan',
+      name: 'Kanpan',
+      component: Kanpan
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/Service',
+      name: 'Service',
+      component: Service
+    },
+    {
+      path: '/Mine',
+      name: 'Mine',
+      component: Mine
     }
   ]
 })
